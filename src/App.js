686e8c98b2a7ad7c5578bcfade138513d9e9ca83
Madiha-Shaikh <madiha.shaikh@crownstack.com>
2022-02-01 19:27:05 +0530

    Create lorem-ipsum project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let number = parseInt(count);
+    if(count <= 0) {
+      number = 1;
+    }
+    if(count > 8) {
+      number = 8;
+    }
+    setParagraphs(data.slice(0, number));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="section-center">
+        <h3>tired of boring lorem ipsum?</h3>
+        <form className="lorem-form">
+          <label>paragraphs:</label>
+          <input type="number" name='number' value={count} onChange={(e) => setCount(e.target.value)} />
+          <button type='submit' className="btn" onClick={handleSubmit}>
+            generate
+          </button>
+        </form>
+        <article>
+          {paragraphs.map((item, index) => {
+            return (
+              <div key={index}>
+                <p>{item}</p>
+              </div>
+            )
+          })}
+        </article>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
